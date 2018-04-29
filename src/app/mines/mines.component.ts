cd5d75693227637945297cc10d04445e064acf37
Aleksandar Mileski <aleksandar.mileski00@gmail.com>
2018-04-29 22:46:13 +0200

    Added modes and code improvements

--- src/app/mines/mines.component.ts
+++ src/app/mines/mines.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs/observable/interval';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-mines',
@@ -6,10 +8,22 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./mines.component.css']
 })
 export class MinesComponent implements OnInit {
-    MODE = {EASY: 'EASY', MEDIUM: 'MEDIUM', HARD: 'HARD'};
+    MODE = { EASY: 'EASY', MEDIUM: 'MEDIUM', HARD: 'HARD' };
+    STATE = { PLAYING: 'PLAYING', WON: 'WON', LOST: 'LOST' };
 
     selectedMode;
     mineMatrix = [];
+    displayMineMatrix = [];
+    shownCounter = 0;
+
+    size = 0;
+    bombsNumber = 0;
+    setBombs = 0;
+    status = this.STATE.PLAYING;
+
+    timer = interval(1000);
+    timerSeconds = 0;
+    timerSubscription: Subscription = null;
 
     constructor() {
     }
@@ -18,43 +32,102 @@ export class MinesComponent implements OnInit {
     }
 
     selectMode(mode) {
+        this.mineMatrix = [];
+        this.displayMineMatrix = [];
         this.selectedMode = mode;
         this.initializeMatrix(this.selectedMode);
+
+        if (this.timerSubscription) this.stopTimer();
+
     }
 
     initializeMatrix(difficulty) {
         this.mineMatrix = [];
-        let size = 0;
-        let bombs = 0;
+
         switch (difficulty) {
             case this.MODE.EASY:
-                size = 9;
-                bombs = 10;
+                this.size = 9;
+                this.bombsNumber = 10;
                 break;
             case this.MODE.MEDIUM:
-                size = 16;
-                bombs = 40;
+                this.size = 16;
+                this.bombsNumber = 40;
                 break;
             case this.MODE.HARD:
-                size = 30;
-                bombs = 100;
+                this.size = 30;
+                this.bombsNumber = 100;
                 break;
             default:
                 break;
         }
 
-        for (let i = 0; i < size; i++) {
-            this.mineMatrix.push(new Array(size).fill('?'));
+        for (let i = 0; i < this.size; i++) {
+            this.mineMatrix.push(new Array(this.size).fill('?'));
+            this.displayMineMatrix.push(new Array(this.size).fill(false));
         }
-        this.addBombs(size, bombs);
+        this.addBombs(this.size, this.bombsNumber);
+        this.shownCounter = 0;
+        this.status = this.STATE.PLAYING;
+        this.setBombs = 0;
+        this.timerSeconds = 0;
+    }
+
+    initializeTimer() {
+        this.timerSubscription = this.timer.subscribe(seconds => this.timerSeconds++);
+    }
+
+    stopTimer() {
+        if (this.timerSubscription) this.timerSubscription.unsubscribe();
+        this.timerSubscription = null;
     }
 
     fieldClick(event, rowIndex, columnIndex) {
-        console.log(event.type === 'click' ? 'CLICK' : 'RIGHTCLICK');
-        console.log(this.mineMatrix[rowIndex][columnIndex]);
-        if (event.type === 'click' && this.mineMatrix[rowIndex][columnIndex] === '?') {
-            this.checkFields(rowIndex, columnIndex);
+        if (!this.timerSubscription && this.timerSeconds === 0) {
+            this.initializeTimer();
         }
+
+        if (this.status === this.STATE.PLAYING) {
+            if (event.type === 'click') {
+
+                if (this.mineMatrix[rowIndex][columnIndex] === '?') {
+                    this.checkFields(rowIndex, columnIndex);
+                }
+                if (this.mineMatrix[rowIndex][columnIndex] === '*'
+                    && this.displayMineMatrix[rowIndex][columnIndex] !== undefined
+                    && this.status === this.STATE.PLAYING) {
+
+                    this.status = this.STATE.LOST;
+                    this.stopTimer();
+                    this.displayMineMatrix.map((row, rowIndx) => {
+                        row.map((field, columnIndx) => {
+                            if (this.mineMatrix[rowIndx][columnIndx] === '*') {
+                                this.displayMineMatrix[rowIndx][columnIndx] = true;
+                            }
+                        });
+                    });
+                }
+                if ("12345678".indexOf(this.mineMatrix[rowIndex][columnIndex]) > -1) {
+                    this.printNeighbours(rowIndex, columnIndex, this.size);
+                }
+
+                if (this.displayMineMatrix[rowIndex][columnIndex] !== undefined) {
+                    this.displayMineMatrix[rowIndex][columnIndex] = true;
+                }
+
+            }
+            if (event.type === 'contextmenu' && !this.displayMineMatrix[rowIndex][columnIndex]) {
+                this.displayMineMatrix[rowIndex][columnIndex] =
+                    this.displayMineMatrix[rowIndex][columnIndex] === undefined
+                        ? false
+                        : undefined;
+                let setBombs = 0;
+                this.displayMineMatrix.forEach(fieldRow => fieldRow.forEach(field => { if (field === undefined) setBombs++; }));
+                this.setBombs = setBombs;
+
+            }
+        }
+
+        this.setDisplayedFields();
         return false;
     }
 
@@ -71,21 +144,55 @@ export class MinesComponent implements OnInit {
     }
 
     setNeighbours(x: number, y: number, size) {
-        // console.log(`Searching around [${x},${y}]`);
         for (let i = x - 1; i <= x + 1; i++) {
             for (let j = y - 1; j <= y + 1; j++) {
                 if (
                     (i >= 0 && i <= size - 1) &&
                     (j >= 0 && j <= size - 1) &&
                     !(i === x && j === y)
-                ) this.setBombsNeighbourIndex(i, j, size);
+                ) { this.setBombsNeighbourIndex(i, j, size); }
             }
         }
-        // console.log(`---------------`);
+    }
+    printNeighbours(x: number, y: number, size) {
+        let fieldNeighbourBombs = this.mineMatrix[x][y];
+        let fieldNeighbourNumbers = [];
+        let fieldNeighbourEmptyFields = [];
+        let foundBombsCounter = 0;
+
+
+        for (let i = x - 1; i <= x + 1; i++) {
+            for (let j = y - 1; j <= y + 1; j++) {
+                if (
+                    (i >= 0 && i <= size - 1) &&
+                    (j >= 0 && j <= size - 1) &&
+                    !(i === x && j === y)
+                ) {
+                    if (this.mineMatrix[i][j] === '*' && this.displayMineMatrix[i][j] === undefined) {
+                        foundBombsCounter++;
+                    }
+
+                    if (this.mineMatrix[i][j] === '?' && !this.displayMineMatrix[i][j]) {
+                        fieldNeighbourEmptyFields = [i, j]
+                    }
+
+                    if (("12345678".indexOf(this.mineMatrix[i][j]) > -1) && (this.displayMineMatrix[i][j] === false)) {
+                        fieldNeighbourNumbers.push([i, j])
+                    }
+                }
+            }
+        }
+
+        if (fieldNeighbourBombs === foundBombsCounter) {
+            if (fieldNeighbourEmptyFields.length) {
+                this.checkFields(fieldNeighbourEmptyFields[0], fieldNeighbourEmptyFields[1]);
+            }
+            fieldNeighbourNumbers.map(field => this.displayMineMatrix[field[0]][field[1]] = true);
+
+        }
     }
 
     setBombsNeighbourIndex(x: number, y: number, size) {
-        // console.log(`[${x},${y}] - '${this.mineMatrix[x][y]}'`);
         let bombsCount = 0;
         for (let i = x - 1; i <= x + 1; i++) {
             for (let j = y - 1; j <= y + 1; j++) {
@@ -104,21 +211,57 @@ export class MinesComponent implements OnInit {
         }
     }
 
-    checkFields(x: number, y: number) {
+    checkFields(x: number, y: number, emptyFieldsArray = [], bombsNumberArray = [], newEmptyNeighboursArray = []) {
         const size = this.mineMatrix.length;
+        let emptyTurnArray = [];
 
         for (let i = x - 1; i <= x + 1; i++) {
             for (let j = y - 1; j <= y + 1; j++) {
                 if (
                     (i >= 0 && i <= size - 1) &&
-                    (j >= 0 && j <= size - 1) &&
-                    !(i === x && j === y)
+                    (j >= 0 && j <= size - 1)
                 ) {
                     if (this.mineMatrix[i][j] === '?') {
+                        let strArr = JSON.stringify(emptyFieldsArray);
+                        let strElement = `[${i},${j}]`;
 
+                        if (strArr.indexOf(strElement) < 0) {
+                            emptyFieldsArray.push([i, j]);
+                            newEmptyNeighboursArray.push([i, j]);
+                        }
+
+                    } else {
+                        let strArr = JSON.stringify(bombsNumberArray);
+                        let strElement = `[${i},${j}]`;
+
+                        if (strArr.indexOf(strElement) < 0) bombsNumberArray.push([i, j])
                     }
                 }
             }
         }
+
+        if (newEmptyNeighboursArray.length === 0) {
+            emptyFieldsArray.forEach(field => this.displayMineMatrix[field[0]][field[1]] = true);
+            bombsNumberArray.forEach(bombNumber => this.displayMineMatrix[bombNumber[0]][bombNumber[1]] = true);
+        } else {
+            let removeElement = newEmptyNeighboursArray.shift();
+            this.checkFields(removeElement[0], removeElement[1], emptyFieldsArray, bombsNumberArray, newEmptyNeighboursArray)
+        }
+
     }
+
+    setDisplayedFields() {
+        this.shownCounter = 0;
+
+        this.displayMineMatrix.forEach(fieldRow => fieldRow.forEach(field => { if (field !== false) this.shownCounter++; }));
+
+        if (
+            (this.shownCounter === (this.mineMatrix.length * this.mineMatrix.length))
+            && (this.setBombs === this.bombsNumber)
+        ) {
+            this.status = this.STATE.WON;
+            this.stopTimer();
+        }
+    }
+
 }
